36df7fce575fd522cb6c04471542f4ba38b078cd
juan <juanpereira.liza@gmail.com>
2025-03-06 21:17:38 -0500

    comit deploy

--- models/Factura.js
+++ models/Factura.js
@@ -1,18 +1,20 @@
 import mongoose from 'mongoose';
 
-const facturaSchema = new mongoose.Schema (
+const facturaSchema = new mongoose.Schema(
     {
-    numbering_range_id: {type: Number, required: true},
-    reference_code: {type: String, required :true, unique: true},
-    observation: { type: String, required: true},
-    paymentForm: { type: String, required: true },
-    paymentDueDate: {type: Date, required: true},
-    paymentMethodCode: {type: String, required: true, unique: true},
-    billingPeriod: {
-        startDate: {type: Date, required: true},
-        StartTime: {type: String, required: true },
-        endDate: { type: Date, required: true},
-        endTime: { type: String, required: true},
-    },  
+        numbering_range_id: { type: Number, required: true },
+        reference_code: { type: String, required: true, unique: true },
+        observation: { type: String, required: true },
+        paymentForm: { type: String, required: true },
+        paymentDueDate: { type: Date, required: true },
+        paymentMethodCode: { type: String, required: true, unique: true },
+        billingPeriod: {
+            startDate: { type: Date, required: true },
+            startTime: { type: String, required: true },
+            endDate: { type: Date, required: true },
+            endTime: { type: String, required: true }
+        }
+    }
+);
 
-export default mongoose.model('factura', facturaSchema)
+export default mongoose.model('factura', facturaSchema);
