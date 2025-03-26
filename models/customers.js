e78e5e6bdaed2d0e022b2760e27d60e783f5b8bc
juan <juanpereira.liza@gmail.com>
2025-03-26 17:15:30 -0500

    commit funcionando productos

--- models/customers.js
+++ models/customers.js
@@ -4,16 +4,17 @@ const customerSchema = new mongoose.Schema(
     {
         legalOrganizationId: { type: String, required: true }, 
         identificationDocumentId: { type: String, required: true },
-        identification: { type: Number, required: true, },
-        dv: { type: String, required: true },
-        company: { type: String, required: true, },
-        tradeName: { type: String, required: true },
-        names: { type: String, required: true },
+        identification: { type: String, required: true, },
+        dv: { type: String},
+        company: { type: String, },
+        tradeName: { type: String, },
+        names: { type: String},
         addres: { type: String, required: true },
-        email: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
         phone: { type: String, required: true },
         tributeId: { type: String, required: true },
-        municipalityId: { type: String, required: true }
+        municipalityId: { type: String, required: true },
+        state: { type: Number, default: 1 },
     })
 
 export default mongoose.model('customers', customerSchema)
--- models/items.js
+++ models/items.js
@@ -4,21 +4,22 @@ const itemsSchema = new mongoose.Schema(
     {
         codeReference: { type: String, required: true, unique: true },
         name: { type: String, required: true },
-        quantity: { type: Number, required: true },
+        quantity: { type: Number },
         discount_rate: { type: Number },
         price: { type: Number, required: true },
-        tax_rate: { type: Number, required: true },
-        unit_measure_id: { type: String, required: true, unique: true },
-        standard_code_id: { type: Number, required: true, unique: true },
+        tax_rate: { type: String, required: true },
+        unit_measure_id: { type: Number, required: true},
+        standard_code_id: { type: String, required: true},
         is_excluded: { type: Boolean, required: true },
-        tribute_id: { type: Number, required: true, unique: true },
-        // withholding_taxes: [
-        //     {
-        //         code: { type: String, required: true, unique: true },
-        //         withholding_taxes_rate: { type: String, required: true },
-        //     },
-        // ]
+        tribute_id: { type: String, required: true },
+        state: { type: Number, default: 1 },
+        withholding_taxes: [
+            {   
+                code: { type: String, },
+                withholding_taxes_rate: { type: String, },
+            },
+        ]
     },
-);  
+);
 
 export default mongoose.model('item', itemsSchema);
