6dde4d14f16bcfb74888547253928e987376d33e
juan <juanpereira.liza@gmail.com>
2025-03-26 17:52:51 -0500

    ajustes productos

--- models/items.js
+++ models/items.js
@@ -10,7 +10,7 @@ const itemsSchema = new mongoose.Schema(
         tax_rate: { type: String, required: true },
         unit_measure_id: { type: Number, required: true},
         standard_code_id: { type: String, required: true},
-        is_excluded: { type: Boolean, required: true },
+        is_excluded: { type: Number, required: true },
         tribute_id: { type: String, required: true },
         state: { type: Number, default: 1 },
         withholding_taxes: [
